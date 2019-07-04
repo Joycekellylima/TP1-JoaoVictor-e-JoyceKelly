eb4405c981c6ab3362f80cef6f2379fdefb5de42
joycekellylima <joycekellyalves7@gmail.com>
2019-07-04 10:27:04 -0300

    cliente

---
+++ Gato/src/main/java/KaBuM/Cliente.java
@@ -0,0 +1,101 @@
+
+package KaBuM;
+
+public class Cliente {
+    
+    private String nome;
+            private int cpf;
+            private int idade;
+            private int rg;
+            private String email;
+            private int senha;
+            
+            public String getNome() {
+                return nome; 
+            }
+            
+            public void setNome (String nome) {
+                    this.nome = nome;
+            
+            public void getcpf () 
+                    return.cpf;
+            
+            public void setcpf (int cpf) {
+                this.setCpf(cpf);
+            }
+
+    /**
+     * @return the cpf
+     */
+    public int getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the idade
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    /**
+     * @return the rg
+     */
+    public int getRg() {
+        return rg;
+    }
+
+    /**
+     * @param rg the rg to set
+     */
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the senha
+     */
+    public int getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+            
+            
+   
+
+    
+}
